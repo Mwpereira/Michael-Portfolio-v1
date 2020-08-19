ca867d846b722a147dbbdb29713cfa192e494696
Michael Pereira <michael.w.pereira.7@gmail.com>
2020-08-19 16:53:00 -0400

    Updating UI including images, text, layout

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const navSlide = () => {
             if(link.style.animation) {
                 link.style.animation = '';
             }else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
         })
         burger.classList.toggle('toggle');
