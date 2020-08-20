74eb91d780922a9172591a798430262f5e6e2b8b
Michael Pereira <michael.w.pereira.7@gmail.com>
2020-08-20 17:18:08 -0400

    Fixing responsive design and media queries

--- index.js
+++ index.js
@@ -72,3 +72,14 @@ $("#Contact-Nav").click(function () {
 	);
 	return false;
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.transform = `translateY(0%)`;
+  } else {
+    document.getElementById("navbar").style.transform = `translateY(-100%)`;
+  }
+  prevScrollpos = currentScrollPos;
+}
