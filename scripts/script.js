517a814ca8f80d5cce3236947c75fb7ab0f42346
Michael Pereira <michael.w.pereira.7@gmail.com>
2020-08-26 16:07:26 -0400

    Formatting files

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,54 @@
 /*----------NavBar Tuning----------*/
 
 const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav-links");
+	const navLinks = document.querySelectorAll(".nav-links li");
 
-    burger.addEventListener("click", () => {
-        nav.classList.toggle("nav-active");
+	burger.addEventListener("click", () => {
+		nav.classList.toggle("nav-active");
 
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = "";
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = "";
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+			}
+		});
 
-        burger.classList.toggle("toggle");
-    });
+		burger.classList.toggle("toggle");
+	});
 };
 
 const navBackground = () => {
-    const nav = document.getElementsByTagName("nav")[0];
+	const nav = document.getElementsByTagName("nav")[0];
 
-    window.addEventListener("scroll", () => {
-        this.scrollY > 200 ? nav.classList.add("is-solid") : nav.classList.remove("is-solid");
-    });
+	window.addEventListener("scroll", () => {
+		this.scrollY > 200 ? nav.classList.add("is-solid") : nav.classList.remove("is-solid");
+	});
 };
 
 const vmwArrow = () => {
-    const button = document.querySelector("#vmwBtn");
+	const button = document.querySelector("#vmwBtn");
 
-    button.addEventListener("click", () => {
-        if ($("#vmwBtn .material-icons").html() == "arrow_right") {
-            $("#vmwBtn .material-icons").html("arrow_drop_down");
-            $("#vmwBtn").css("background-color", "rgb(76, 18, 61)");
-            document.getElementById("About-Nav").click();
-        } else {
-            $("#vmwBtn .material-icons").html("arrow_right");
+	button.addEventListener("click", () => {
+		if ($("#vmwBtn .material-icons").html() == "arrow_right") {
+			$("#vmwBtn .material-icons").html("arrow_drop_down");
+			$("#vmwBtn").css("background-color", "rgb(76, 18, 61)");
+			document.getElementById("About-Nav").click();
+		} else {
+			$("#vmwBtn .material-icons").html("arrow_right");
 			$("#vmwBtn").css("background-color", "transparent");
 			document.getElementById("Home-Nav").click();
-        }
-    });
+		}
+	});
 };
 
 $(".nav-links li a").click(function () {
-    const nav = document.querySelector(".nav-links");
-    if (nav.classList.contains("nav-active")) {
-        document.getElementsByClassName("burger")[0].click();
-    }
+	const nav = document.querySelector(".nav-links");
+	if (nav.classList.contains("nav-active")) {
+		document.getElementsByClassName("burger")[0].click();
+	}
 });
 
 navSlide();
@@ -61,63 +61,62 @@ vmwArrow();
 
 let scrollMargin = 0;
 
-if(window.innerWidth > 1600){
-    scrollMargin = 125;
-}
-else{
-    scrollMargin = window.innerHeight*.15;
+if (window.innerWidth > 1600) {
+	scrollMargin = 125;
+} else {
+	scrollMargin = window.innerHeight * 0.15;
 }
 
 $("#Home-Nav").addClass("active");
 
 $("#Home-Nav").click(function () {
-    $("html, body").animate(
-        {
-            scrollTop: $("#Home").offset().top,
-        },
-        1000
-    );
-    return false;
+	$("html, body").animate(
+		{
+			scrollTop: $("#Home").offset().top,
+		},
+		1000
+	);
+	return false;
 });
 
 $("#About-Nav").click(function () {
-    $("html, body").animate(
-        {
-            scrollTop: $("#About").offset().top - scrollMargin,
-        },
-        1000
-    );
-    return false;
+	$("html, body").animate(
+		{
+			scrollTop: $("#About").offset().top - scrollMargin,
+		},
+		1000
+	);
+	return false;
 });
 
 $("#Skills-Nav").click(function () {
-    $(" html,body").animate(
-        {
-            scrollTop: $("#Skills").offset().top - scrollMargin,
-        },
-        1000
-    );
-    return false;
+	$(" html,body").animate(
+		{
+			scrollTop: $("#Skills").offset().top - scrollMargin,
+		},
+		1000
+	);
+	return false;
 });
 
 $("#Portfolio-Nav").click(function () {
-    $(this).addClass("active");
-    $("html,body ").animate(
-        {
-            scrollTop: $("#Portfolio").offset().top - scrollMargin,
-        },
-        1000
-    );
-    return false;
+	$(this).addClass("active");
+	$("html,body ").animate(
+		{
+			scrollTop: $("#Portfolio").offset().top - scrollMargin,
+		},
+		1000
+	);
+	return false;
 });
 
 $("#Contact-Nav").click(function () {
-    $(this).addClass("active");
-    $("html,body ").animate(
-        {
-            scrollTop: $("#Contact").offset().top - scrollMargin,
-        },
-        1000
-    );
-    return false;
+	$(this).addClass("active");
+	$("html,body ").animate(
+		{
+			scrollTop: $("#Contact").offset().top - scrollMargin,
+		},
+		1000
+	);
+	return false;
 });
