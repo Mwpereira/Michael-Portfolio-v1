e170ef8ce9d09c698610ad5ce6ac892169b4c91b
Michael Pereira <michael.w.pereira.7@gmail.com>
2020-08-25 20:15:32 -0400

    Fixing NavBar

--- index.js
+++ index.js
@@ -52,6 +52,74 @@ navBackground();
 
 vmwArrow();
 
+
+// cache the navigation links 
+var $navigationLinks = $('#navigation > ul > li > a');
+// cache (in reversed order) the sections
+var $sections = $($(".section").get().reverse());
+
+// map each section id to their corresponding navigation link
+var sectionIdTonavigationLink = {};
+$sections.each(function() {
+    var id = $(this).attr('id');
+    sectionIdTonavigationLink[id] = $('#navigation > ul > li > a[href=\\#' + id + ']');
+});
+
+// throttle function, enforces a minimum time interval
+function throttle(fn, interval) {
+    var lastCall, timeoutId;
+    return function () {
+        var now = new Date().getTime();
+        if (lastCall && now < (lastCall + interval) ) {
+            // if we are inside the interval we wait
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(function () {
+                lastCall = now;
+                fn.call();
+            }, interval - (now - lastCall) );
+        } else {
+            // otherwise, we directly call the function 
+            lastCall = now;
+            fn.call();
+        }
+    };
+}
+
+function highlightNavigation() {
+    // get the current vertical position of the scroll bar
+    var scrollPosition = $(window).scrollTop();
+
+    // iterate the sections
+    $navbar.each(function() {
+        var currentSection = $(this);
+        // get the position of the section
+        var sectionTop = currentSection.offset().top;
+
+        // if the user has scrolled over the top of the section  
+        if (scrollPosition >= sectionTop) {
+            // get the section id
+            var id = currentSection.attr('id');
+            // get the corresponding navigation link
+            var $navigationLink = sectionIdTonavigationLink[id];
+            // if the link is not active
+            if (!$navigationLink.hasClass('active')) {
+                // remove .active class from all the links
+                $navigationLinks.removeClass('active');
+                // add .active class to the current link
+                $navigationLink.addClass('active');
+            }
+            // we have found our section, so we return false to exit the each loop
+            return false;
+        }
+    });
+}
+
+$(window).scroll( throttle(highlightNavigation,100) );
+
+// if you don't want to throttle the function use this instead:
+// $(window).scroll( highlightNavigation );
+
+
 /*********** Responsive Scroll ***********/
 
 var pxH = window.innerHeight;
